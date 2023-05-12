6aa6d100a1e2d1c911ba8a147d2448d096bfca94
Skirt <126694961+skirtsnetwork@users.noreply.github.com>
2023-05-12 16:01:02 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,61 +1,37 @@
-import createBareServer from "@tomphttp/bare-server-node";
-import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+var hash = window.location.hash;
+if(hash){
 
-import http from "node:http";
-import serveStatic from "serve-static";
-import connect from "connect";
-import Buffy from "./buffy.js";
+    var url  = hash.replace('#','');
+    openSite(`https://html5.gamedistribution.com/rvvASMiM/${url}/?gd_sdk_referrer_url=https://gamedistribution.com/games/crazy-dunk`);
+}
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
-console.log(`
-██████╗ ██╗   ██╗███████╗███████╗██╗   ██╗
-██╔══██╗██║   ██║██╔════╝██╔════╝╚██╗ ██╔╝
-██████╔╝██║   ██║█████╗  █████╗   ╚████╔╝ 
-██╔══██╗██║   ██║██╔══╝  ██╔══╝    ╚██╔╝  
-██████╔╝╚██████╔╝██║     ██║        ██║   
-╚═════╝  ╚═════╝ ╚═╝     ╚═╝        ╚═╝                                          
-`);
 
-// The following message MAY NOT be removed
-console.log("Incognito-Lite\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
-
-const proxy = new Buffy({
-  url: "https://amethystnetwork-dev.github.io/Incognito/static",
-  validateStatus: (status) => status !== 404
-}); // Proxy the static folder
-const gs = new Buffy({
-  url: "https://amethystnetwork-dev.github.io/Incognito/gsource",
-  validateStatus: (status) => status !== 404
-}); // Proxy the game files
-const app = connect();
-const bare = createBareServer("/bare/");
-const server = http.createServer();
-
-app.use((req, res, next) => {
-  if(bare.shouldRoute(req)) bare.routeRequest(req, res); else next();
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
 });
+function openSite(url){
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        // let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
-app.use("/service", (req, res) => res.end("OK"));
-app.use("/source", (req, res, next) => gs.request(req, res, next));
-app.use((req, res, next) => proxy.request(req, res, next));
 
-app.use("/uv", serveStatic(uvPath));
-
-server.on("request", app);
-
-server.on("upgrade", (req, socket, head) => {
-  if(bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
-});
-
-server.on("listening", () => {
-  const addr = server.address();
-
-  console.log(`Server running on port ${addr.port}`);
-  console.log("");
-  console.log("You can now view it in your browser.");
-  /* Code for listing IPS from website-aio */
-  console.log(`Local: http://${addr.family === "IPv6" ? `[${addr.address}]` : addr.address}:${addr.port}`);
-  try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
-  if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
-});
-
-server.listen({ port: (process.env.PORT || 8080) })
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+}
+function isUrl(val = ''){
+    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+    return false;
+};
