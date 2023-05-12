1bca6ac0c0e18e7585d67eed3844d66670b1483b
Skirt <126694961+skirtsnetwork@users.noreply.github.com>
2023-05-12 11:05:18 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-  prefix: "/uv/service/",
+  prefix: "/uv/school/",
   bare: "https://incog.dev/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
